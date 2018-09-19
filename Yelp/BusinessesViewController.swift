956f2588b604c4919b01dd9075bb7bce89ba1883
claudiaareneee <claudiaareneee@gmail.com>
2018-09-19 18:10:40 -0500

    added .gitattributes

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
     @IBOutlet weak var tableView: UITableView!
+    
     var businesses: [Business]!
     
     override func viewDidLoad() {
