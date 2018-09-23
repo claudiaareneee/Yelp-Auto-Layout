b914e6e4cdf4de8e031990ba149c1c493e220da7
claudiaareneee <claudiaareneee@gmail.com>
2018-09-23 18:21:52 -0500

    adding search result (has an error)

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,26 +8,49 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating{
     
     @IBOutlet weak var tableView: UITableView!
     
     var searchController: UISearchController!
+    
     var businesses: [Business]!
-//    var filteredBusinesses: [Business]!
+    var filteredData: [Business]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
         
+        
+        filteredData = businesses
+        
+        // Initializing with searchResultsController set to nil means that
+        // searchController will use this view controller to display the search results
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        
+        // If we are using this same view controller to present the results
+        // dimming it out wouldn't make sense. Should probably only set
+        // this to yes if using another controller to display the search results.
+        searchController.dimsBackgroundDuringPresentation = false
+        
+        searchController.searchBar.sizeToFit()
+        navigationItem.titleView = searchController.searchBar
+        searchController.hidesNavigationBarDuringPresentation = false
+        
+        // Sets this view controller as presenting view controller for the search interface
+        definesPresentationContext = true
+        
+        
         // Collecting businesses
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
-                self.businesses = businesses
+                self.filteredData = businesses
                 self.tableView.reloadData()
                 if let businesses = businesses {
                     for business in businesses {
@@ -39,8 +62,6 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
             }
         )
         
-//        filteredBusinesses = businesses
-        
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm(term: "Restaurants", sort: .distance, categories: ["asianfusion", "burgers"]) { (businesses, error) in
                 self.businesses = businesses
@@ -50,12 +71,11 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
                  }
          }
          */
-        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if businesses != nil {
-            return businesses!.count
+        if filteredData != nil {
+            return filteredData!.count
         } else {
             return 0
         }
@@ -64,10 +84,27 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
         
-        cell.business = businesses[indexPath.row]
+        cell.business = filteredData[indexPath.row]
         
         return cell
     }
+    
+    //HELP!!
+    //I am getting an error here. My app builds and runs, but when I go to search, it crashes with the error: "Thread 1: Fatal error: Unexpectedly found nil while unwrapping optional value"
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            Business.searchWithTerm(term: searchText, completion: {
+                (businesses: [Business]?, error: Error?) -> Void in
+                
+                self.filteredData = businesses
+                self.tableView.reloadData()
+                })
+        }
+    }
+
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
